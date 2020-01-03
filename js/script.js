9cbe19fe2286fa198017b5fc8c922d8008b2a63b
tac0kat <bln0004@auburn.edu>
2020-01-03 18:52:54 +0700

    made ma rocket

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@ var sayMyName = function (name) {
 	alert('My name is:' +name)
 }
 
+var doCoolStuff = function (){
+	var currentClassName = document.getElementById('cool').className;
+	if(currentClassName == 'cool'){document.getElementById('cool').className = 'cool red';}
+	else{ 
+		document.getElementById('cool').className = 'cool';}
 
+
+}
 
 var car = {
 			make: 'VW',
